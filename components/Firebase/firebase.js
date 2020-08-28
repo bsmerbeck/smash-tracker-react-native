822360310f22406f09f6d63518f1231fa9282699
Brenden Smerbeck <bsmerbeck@gmail.com>
2020-08-28 17:16:00 -0400

    Initial commit, working authentication with google

--- components/Firebase/firebase.js
+++ components/Firebase/firebase.js
@@ -1,7 +1,17 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/auth';
+import * as Google from 'expo-google-app-auth';
 
 import firebaseConfig from './firebaseConfig';
+
+
+const config = {
+  //webClientId: `781901075636-5b15tlgfl4omnqsglceuatvah9te9c3l.apps.googleusercontent.com`,
+  // iosClientId: `<YOUR_IOS_CLIENT_ID>`,
+  androidClientId: `781901075636-5b15tlgfl4omnqsglceuatvah9te9c3l.apps.googleusercontent.com`,
+  // iosStandaloneAppClientId: `<YOUR_IOS_CLIENT_ID>`,
+  //androidStandaloneAppClientId: `781901075636-5b15tlgfl4omnqsglceuatvah9te9c3l.apps.googleusercontent.com`,
+};
 
 // Initialize Firebase App
 
@@ -16,6 +26,13 @@ export const loginWithEmail = (email, password) =>
 
 export const registerWithEmail = (email, password) =>
   auth.createUserWithEmailAndPassword(email, password);
+
+export const loginWithGmail = async () => {
+  const {type, accessToken} = await Google.logInAsync(config)
+  if (type === 'success'){
+    await Google.logOutAsync({accessToken, ...config});
+  }
+}
 
 export const logout = () => auth.signOut();
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 
 import useStatusBar from '../hooks/useStatusBar';
 import { logout } from '../components/Firebase/firebase';
+import {AuthUserContext} from "../navigation/AuthUserProvider";
+import firebase from 'firebase';
 
 export default function HomeScreen() {
   useStatusBar('dark-content');
+  const { user, setUser } = useContext(AuthUserContext);
   async function handleSignOut() {
     try {
       await logout();
@@ -16,6 +19,16 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Button title="Sign Out" onPress={handleSignOut} />
+      <Button title="loadFighters" onPress={() => {
+        if(!user){
+          return;
+        } else {
+          firebase.database().ref('/matches/' + user.uid).once('value')
+            .then((snapshot) => {
+              console.log(JSON.stringify(snapshot))
+            })
+        }
+      }}/>
     </View>
   );
 }
